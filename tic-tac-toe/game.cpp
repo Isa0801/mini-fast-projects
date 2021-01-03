d352cf94f14749d37733287040cec7c8e282d280
isa0801 <isa080199@gmail.com>
2021-01-03 21:26:53 +0100

    adding online aspect

--- tic-tac-toe/game.cpp
+++ tic-tac-toe/game.cpp
@@ -88,51 +88,80 @@ public:
 class TicTaceToe
 {
 private:
+sf::TcpSocket socket;
+unsigned short port;
+sf::IpAddress ip;
+bool host = false;
 std::vector<std::shared_ptr<baseObj>> gameObj;
 std::map<std::string, sf::RectangleShape> baseGObj;
 int H,W;
-bool player1Turn = false;
-bool player2Turn = false;
+bool yourTurn = false, yourX = false;
 int gameMap[3][3] = {{-1, -1, -1},
                     {-1, -1, -1},           
                     {-1, -1, -1}
                                 };
-int xTurn = 1;
+int Turns = 1;
 public:
-    TicTaceToe(sf::Vector2f, sf::Vector2f, float);
+    TicTaceToe(sf::Vector2f, sf::Vector2f, float, sf::IpAddress, unsigned short, bool);
     ~TicTaceToe();
     bool winCond();
+    void dealWithNetwork(int, sf::Vector2i, bool );
     void render(sf::RenderWindow&);
     void update(sf::RenderWindow&);
 };
 
-TicTaceToe::TicTaceToe(sf::Vector2f windowSize, sf::Vector2f boxSize, float borderThickness){
+TicTaceToe::TicTaceToe(sf::Vector2f windowSize, sf::Vector2f boxSize, float borderThickness, sf::IpAddress ip, unsigned short port, bool host){
 
-sf::RectangleShape box(sf::Vector2f(boxSize.x, boxSize.y));
-box.setOrigin(sf::Vector2f(boxSize.x/2.f, boxSize.y/2.f));
-box.setPosition(sf::Vector2f(windowSize.x/2.f, windowSize.y/2.f));
-baseGObj["box"] = box;
+    this->host = host;
+    this->ip = ip;
+    this->port = port;
 
-// first line up
-sf::RectangleShape line(sf::Vector2f(borderThickness, box.getSize().y));
-line.setOrigin(sf::Vector2f(borderThickness/2.f, box.getSize().y/2.f));
-line.setPosition(sf::Vector2f(box.getPosition().x - box.getSize().x/6.f, box.getPosition().y));
-line.setFillColor(sf::Color::Black);
-baseGObj["up1"] = line;
+    if(host){
+        
+        sf::TcpListener listener;
+        listener.listen(port);
+        listener.accept(socket);
 
-// second line up
-line.setPosition(sf::Vector2f(box.getPosition().x + box.getSize().x/6.f, box.getPosition().y));
-baseGObj["up2"] = line;
+        yourTurn = true;
+        yourX = true;
 
-// first line to right
-line = sf::RectangleShape(sf::Vector2f(box.getSize().x, borderThickness));
-line.setOrigin(sf::Vector2f(box.getSize().x/2.f, borderThickness/2.f));
-line.setPosition(sf::Vector2f(box.getPosition().x, box.getPosition().y - box.getSize().y/6.f));
-line.setFillColor(sf::Color::Black);
-baseGObj["right1"] = line;
-// second line to right
-line.setPosition(sf::Vector2f(box.getPosition().x, box.getPosition().y + box.getSize().y/6.f));
-baseGObj["right2"] = line;
+    } else
+    {
+        socket.connect(ip, port);
+        yourTurn = false;
+        yourX = false;
+    }
+    
+
+    socket.setBlocking(false);
+
+    sf::sleep(sf::milliseconds(1000));
+
+    sf::RectangleShape box(sf::Vector2f(boxSize.x, boxSize.y));
+    box.setOrigin(sf::Vector2f(boxSize.x/2.f, boxSize.y/2.f));
+    box.setPosition(sf::Vector2f(windowSize.x/2.f, windowSize.y/2.f));
+    baseGObj["box"] = box;
+
+    // first line up
+    sf::RectangleShape line(sf::Vector2f(borderThickness, box.getSize().y));
+    line.setOrigin(sf::Vector2f(borderThickness/2.f, box.getSize().y/2.f));
+    line.setPosition(sf::Vector2f(box.getPosition().x - box.getSize().x/6.f, box.getPosition().y));
+    line.setFillColor(sf::Color::Black);
+    baseGObj["up1"] = line;
+
+    // second line up
+    line.setPosition(sf::Vector2f(box.getPosition().x + box.getSize().x/6.f, box.getPosition().y));
+    baseGObj["up2"] = line;
+
+    // first line to right
+    line = sf::RectangleShape(sf::Vector2f(box.getSize().x, borderThickness));
+    line.setOrigin(sf::Vector2f(box.getSize().x/2.f, borderThickness/2.f));
+    line.setPosition(sf::Vector2f(box.getPosition().x, box.getPosition().y - box.getSize().y/6.f));
+    line.setFillColor(sf::Color::Black);
+    baseGObj["right1"] = line;
+    // second line to right
+    line.setPosition(sf::Vector2f(box.getPosition().x, box.getPosition().y + box.getSize().y/6.f));
+    baseGObj["right2"] = line;
 
 }
 
@@ -211,6 +240,31 @@ bool TicTaceToe::winCond(){
 
 }
 
+
+void TicTaceToe::dealWithNetwork(int type, sf::Vector2i which, bool send){
+
+    sf::Packet packet;
+
+    
+ 
+    if (send){
+        std::cout << "sending: " << type << " at " << which.x << " " << which.y << "\n";
+        packet << type << which.x << which.y;
+        socket.send(packet);
+        yourTurn = !yourTurn;
+    } else {
+
+    socket.receive(packet);
+
+        if (packet >> type >> which.x >> which.y){
+            yourTurn = !yourTurn;
+            std::cout << "got: " << type << " at " << which.x << " " << which.y << "\n";
+            gameMap[which.x][which.y] = type;
+        }
+    }
+
+}
+
 void TicTaceToe::render(sf::RenderWindow& window){
 
     // render base layout
@@ -219,10 +273,73 @@ void TicTaceToe::render(sf::RenderWindow& window){
     }
 
     // render main obj
-    for (auto& obj: gameObj){
-        //window.draw(obj);
-        obj->render(window);
+    // for (auto& obj: gameObj){
+    //     //window.draw(obj);
+    //     obj->render(window);
+    // }
+
+
+
+    for (size_t x = 0; x <= 2; x++){
+
+        sf::Vector2f cords;
+        sf::Vector2f size;
+
+        switch (x)
+        {
+        case 0:
+            cords.x = baseGObj["box"].getPosition().x - baseGObj["box"].getSize().x/3;
+            size.x = ( baseGObj["up1"].getPosition().x - (baseGObj["box"].getPosition().x - baseGObj["box"].getSize().x/2));
+            size.x = abs(size.x); 
+            break;
+            
+        case 1:
+            cords.x = baseGObj["box"].getPosition().x;
+            size.x = (baseGObj["up2"].getPosition().x - baseGObj["up1"].getPosition().x);
+            size.x = abs(size.x);
+            break;
+        
+        case 2:
+            cords.x = baseGObj["box"].getPosition().x + baseGObj["box"].getSize().x/3;
+            size.x = ((baseGObj["box"].getPosition().x + baseGObj["box"].getSize().x/2) - baseGObj["up2"].getPosition().x); 
+            size.x = abs(size.x);
+            break;
+        }
+        
+        // top pos
+        cords.y = baseGObj["box"].getPosition().y - baseGObj["box"].getSize().y/3;
+        size.y =  baseGObj["right1"].getPosition().y - (baseGObj["box"].getPosition().y - baseGObj["box"].getSize().y/2) ;
+        size.y = abs(size.y);
+
+        if (gameMap[x][0] == 1)
+            xObj(size, cords).render(window);
+        else if (gameMap[x][0] == 2)
+            cirObj(size, cords).render(window);
+
+
+        // mid pos
+        cords.y = baseGObj["box"].getPosition().y;
+        size.y = (baseGObj["box"].getPosition().y +  baseGObj["box"].getSize().y/2) - baseGObj["right2"].getPosition().y;
+        size.y = abs(size.y);
+
+
+        if (gameMap[x][1] == 1)
+            xObj(size, cords).render(window);
+        else if (gameMap[x][1] == 2)
+            cirObj(size, cords).render(window);
+        
+        // bottom pos
+
+        cords.y = baseGObj["box"].getPosition().y + baseGObj["box"].getSize().y/3;
+        size.y = ((baseGObj["box"].getPosition().y + baseGObj["box"].getSize().y/2) - baseGObj["right2"].getPosition().y); 
+        size.y = abs(size.y);
+
+        if (gameMap[x][2] == 1)
+            xObj(size, cords).render(window);
+        else if (gameMap[x][2] == 2)
+            cirObj(size, cords).render(window);
     }
+    
     
 
 }
@@ -239,7 +356,8 @@ void TicTaceToe::update(sf::RenderWindow& window){
                 gameMap[i][1] = -1;
                 gameMap[i][2] = -1;
 
-                xTurn = 1;
+                Turns = 1;
+                yourX = !yourX;
                 sf::sleep(sf::milliseconds(1000));
             }
             
@@ -255,28 +373,16 @@ void TicTaceToe::update(sf::RenderWindow& window){
 
         //left side
         if (mousePosF.x > baseGObj["box"].getPosition().x - baseGObj["box"].getSize().x/2 && mousePosF.x < baseGObj["up1"].getPosition().x ){
-
-            cords.x = baseGObj["box"].getPosition().x - baseGObj["box"].getSize().x/3;
-            size.x = ( baseGObj["up1"].getPosition().x - (baseGObj["box"].getPosition().x - baseGObj["box"].getSize().x/2));
-            size.x = abs(size.x); 
             x = 0;
             //std::cout << "left side\n";
         }
         //middle side
         else if (mousePosF.x > baseGObj["up1"].getPosition().x && mousePosF.x < baseGObj["up2"].getPosition().x){
-
-            cords.x = baseGObj["box"].getPosition().x;
-            size.x = (baseGObj["up2"].getPosition().x - baseGObj["up1"].getPosition().x);
-            size.x = abs(size.x); 
             x = 1;
             //std::cout << "middle side\n";
         }
         //right side
         else if (mousePosF.x > baseGObj["up2"].getPosition().x && mousePosF.x < baseGObj["box"].getPosition().x + baseGObj["box"].getSize().x/2){
-
-            cords.x = baseGObj["box"].getPosition().x + baseGObj["box"].getSize().x/3;
-            size.x = ((baseGObj["box"].getPosition().x + baseGObj["box"].getSize().x/2) - baseGObj["up2"].getPosition().x); 
-            size.x = abs(size.x);
 
             x = 2;
             //std::cout << "right side\n";
@@ -286,30 +392,19 @@ void TicTaceToe::update(sf::RenderWindow& window){
     
         //top side
         if (mousePosF.y >  baseGObj["box"].getPosition().y - baseGObj["box"].getSize().y/2 && mousePosF.y < baseGObj["right1"].getPosition().y ){
-            
-            cords.y = baseGObj["box"].getPosition().y - baseGObj["box"].getSize().y/3;
-            size.y =  baseGObj["right1"].getPosition().y - (baseGObj["box"].getPosition().y - baseGObj["box"].getSize().y/2) ;
-            size.y = abs(size.y);
-
             y = 0;
             //std::cout << "top side\n";
         }
         // middle side
         else if (mousePosF.y > baseGObj["right1"].getPosition().y && mousePosF.y < baseGObj["right2"].getPosition().y){
-            
-            cords.y = baseGObj["box"].getPosition().y;
-            size.y = (baseGObj["box"].getPosition().y +  baseGObj["box"].getSize().y/2) - baseGObj["right2"].getPosition().y;
-            size.y = abs(size.y);
+
     
             y = 1;
             //std::cout << "middle side\n";
         } 
         // bottom side
         else if (mousePosF.y > baseGObj["right2"].getPosition().y && mousePosF.y < baseGObj["box"].getPosition().y + baseGObj["box"].getSize().y/2){
-            
-            cords.y = baseGObj["box"].getPosition().y + baseGObj["box"].getSize().y/3;
-            size.y = ((baseGObj["box"].getPosition().y + baseGObj["box"].getSize().y/2) - baseGObj["right2"].getPosition().y); 
-            size.y = abs(size.y);
+
             y = 2;
             //std::cout << "bottom side\n";
         } else
@@ -319,26 +414,20 @@ void TicTaceToe::update(sf::RenderWindow& window){
 
         //std::cout << "x: " << x << " y: " << y << std::endl;
 
-        if (gameMap[x][y] == -1){
+        if (gameMap[x][y] == -1 && yourTurn){
             
 
-            std::shared_ptr<baseObj> temp;
-
-            if (xTurn%2 != 0){
-                temp = std::make_shared<xObj>(size, cords, sf::Color::Green);
+            if (yourX){
                 gameMap[x][y] = 1;
             } else {
-                temp = std::make_shared<cirObj>(size, cords, sf::Color::Red);
                 gameMap[x][y] = 2;
             }
             // sf::RectangleShape temp(size);
             // temp.setOrigin(sf::Vector2f(size.x/2, size.y/2));
             // temp.setPosition(cords);
             // temp.setFillColor(sf::Color::Green);
-
-            gameObj.push_back(temp);
-
-            xTurn++;
+            dealWithNetwork(gameMap[x][y], sf::Vector2i(x,y), true);
+            Turns++;
         }
 
     }
@@ -351,7 +440,34 @@ int main()
     sf::Vector2f winodwSize(1920, 1080);
     
     sf::RenderWindow window(sf::VideoMode(winodwSize.x, winodwSize.y), "SFML works!");
-    TicTaceToe game(winodwSize, sf::Vector2f(1280, 720), 2.5f);
+
+    char connectionType;
+	std::cout << "enter s for server and c for client: ";
+	std::cin >> connectionType;
+
+    bool host = false;
+    unsigned short port;
+    sf::IpAddress ip;
+
+	if (connectionType == 's'){
+        host = true;
+        std::cout << "Enter port: ";
+        std::cin >> port;
+    }
+	else
+	{
+        host = false;
+		std::string ipToConnect;
+		std::cout << "Enter ip to connect to: ";
+		std::cin >> ipToConnect;
+        ip = sf::IpAddress(ipToConnect);
+        std::cout << "Enter port: ";
+        std::cin >> port;
+	
+    }
+
+
+    TicTaceToe game(winodwSize, sf::Vector2f(1280, 720), 2.5f, ip, port, host);
 
     while (window.isOpen())
     {
@@ -364,6 +480,7 @@ int main()
         
         window.clear();
         game.update(window);
+        game.dealWithNetwork(-1, sf::Vector2i(0,0), false);
         game.render(window);
         window.display();
     }
